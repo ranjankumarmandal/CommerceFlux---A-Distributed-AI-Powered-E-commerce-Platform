0fd9d96f9f06d23a26e5be3a94553f46e3369853
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-20 17:19:25 +0530

    CommerceFlux: identity-service: prod jwt refresh development changes: JwtService

---
+++ identity-service/src/main/java/com/rnajankumarmandal/identity_service/service/JwtService.java
@@ -0,0 +1,49 @@
+package com.rnajankumarmandal.identity_service.service;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET =
+            "commercefluxcommercefluxcommercefluxcommerceflux";
+
+    private final SecretKey key =
+            Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 15))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isValid(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}

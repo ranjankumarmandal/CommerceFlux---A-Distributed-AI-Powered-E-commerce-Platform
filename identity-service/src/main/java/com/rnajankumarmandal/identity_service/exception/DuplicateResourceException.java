d1aaaacb6cba5423f5910c30cee7544421ef1c3a
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-20 16:43:46 +0530

    CommerceFlux: exception: DuplicateResourceException

---
+++ identity-service/src/main/java/com/rnajankumarmandal/identity_service/exception/DuplicateResourceException.java
@@ -0,0 +1,8 @@
+package com.rnajankumarmandal.identity_service.exception;
+
+public class DuplicateResourceException extends RuntimeException {
+
+    public DuplicateResourceException(String message) {
+        super(message);
+    }
+}

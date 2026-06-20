d0fe9f02bb64d92fbadf49f86c6b234025541051
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-20 17:22:45 +0530

    CommerceFlux: identity-service: prod jwt refresh development changes: config

--- identity-service/src/main/java/com/rnajankumarmandal/identity_service/config/SecurityConfig.java
+++ identity-service/src/main/java/com/rnajankumarmandal/identity_service/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.rnajankumarmandal.identity_service.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +13,20 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/auth/register",
+                                "/api/v1/auth/login"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
     }
 }

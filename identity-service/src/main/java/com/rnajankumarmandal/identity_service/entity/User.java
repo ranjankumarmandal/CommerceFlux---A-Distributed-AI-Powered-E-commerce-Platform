d3c43e9b2b555387b97052e63597cc9258609161
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-20 16:37:08 +0530

    CommerceFlux: entity: completed class User

---
+++ identity-service/src/main/java/com/rnajankumarmandal/identity_service/entity/User.java
@@ -0,0 +1,36 @@
+package com.rnajankumarmandal.identity_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    private Instant createdAt;
+
+    private Instant updatedAt;
+}

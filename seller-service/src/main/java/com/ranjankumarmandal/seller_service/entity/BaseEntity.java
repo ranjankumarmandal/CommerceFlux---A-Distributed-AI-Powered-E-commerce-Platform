d0de6b125b77fc2eaee3b8e841e869835f61b3fb
ranjankumarmandal <ranjancit2k15cse@gmail.com>
2026-06-27 09:39:10 +0530

    CommerceFlux: Seller-Service: entity1

---
+++ seller-service/src/main/java/com/ranjankumarmandal/seller_service/entity/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.ranjankumarmandal.seller_service.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
